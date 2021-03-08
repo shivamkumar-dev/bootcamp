6d88dc178c5a6381c3011ca3cf1cfe46fe8e4e33
Shivam Kumar <shivam66.sk@gmail.com>
2021-03-08 22:14:18 +0530

    async handler added

--- middleware/error.js
+++ middleware/error.js
@@ -4,10 +4,24 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
 
+  // console.log(err);
+
   // Mongoose bad ObjectId
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose Duplicate Key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose Validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
