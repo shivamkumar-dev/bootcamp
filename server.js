9fca5a92f3266b17ea77312f0fe49c4ad787f002
shivam <shivam@signitysolutions.com>
2022-01-03 19:07:23 +0530

    feature user login, logout and protect route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const fileupload = require('express-fileupload');
 const app = express();
 const errorHandler = require('./middleware/error');
@@ -11,6 +12,7 @@ connectDB();
 // routes
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 // fileupload
 app.use(fileupload());
@@ -20,8 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // mount routes & other middlewares
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
